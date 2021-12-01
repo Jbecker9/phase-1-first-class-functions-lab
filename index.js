4176a77c2490cd7fc06512fa5e452eaab0f71398
Jared <jbeckersalax9@aol.com>
2021-12-01 13:44:23 -0500

    Done.

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ const fareDoubler = createFareMultiplier(2);
 const fareTripler = createFareMultiplier(3);
 
 const selectDifferentDrivers = function(drivers, selectingDrivers){
-    return selectDifferentDrivers(selectingDrivers[0], selectingDrivers[1]);
+    return selectingDrivers(drivers)
 };
 
