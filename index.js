960138e33c37468614f85bc06aab8b2914985320
Jared <jbeckersalax9@aol.com>
2021-11-30 23:00:02 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-const returnFirstTwoDrivers = function(){}
-//     return drivers[0], drivers[1];
+const returnFirstTwoDrivers = function(drivers){
+    return Array(drivers[0], drivers[1]);
+};
 
+const returnLastTwoDrivers = function(drivers){
+    return Array(drivers[2], drivers[3]);
+};
+
+function selectingDrivers(){
+    Array[
+        [returnFirstTwoDrivers(selectingDrivers)],
+        [returnLastTwoDrivers(selectingDrivers)]
+    ]
+}
